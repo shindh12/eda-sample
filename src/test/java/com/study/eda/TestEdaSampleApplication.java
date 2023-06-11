1d3417038387a4670e6a6b067819fe790bbfcb9a
shindh12 <shindh159@gmail.com>
2023-06-11 13:16:25 +0900

    first commit

---
+++ src/test/java/com/study/eda/TestEdaSampleApplication.java
@@ -0,0 +1,22 @@
+package com.study.eda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestEdaSampleApplication {
+
+	@Bean
+	@ServiceConnection
+	MySQLContainer<?> mysqlContainer() {
+		return new MySQLContainer<>("mysql:latest");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(BookLibraryApplication::main).with(TestEdaSampleApplication.class).run(args);
+	}
+
+}
