9156648eff916f4bc5f273ced1c2bdc8e3d492c2
shindh12 <shindh159@gmail.com>
2023-06-18 16:56:53 +0900

    Application 클래스 패키지 변경

--- src/main/java/com/study/eda/BookLibraryApplication.java
+++ src/main/java/com/study/eda/BookLibraryApplication.java
@@ -1,4 +1,4 @@
-package com.study.eda.library;
+package com.study.eda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/study/eda/BookLibraryApplicationTests.java
+++ src/test/java/com/study/eda/BookLibraryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.study.eda.library;
+package com.study.eda;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
