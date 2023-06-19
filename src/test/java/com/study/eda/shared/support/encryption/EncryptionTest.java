7dfdb29fa6247b753a78d94f26b1b0d006cfc8da
shindh12 <shindh159@gmail.com>
2023-06-19 20:37:54 +0900

    Encryption 모듈 테스트코드 수정

--- src/test/java/com/study/eda/shared/support/encryption/EncryptionTest.java
+++ src/test/java/com/study/eda/shared/support/encryption/EncryptionTest.java
@@ -7,33 +7,62 @@ import java.io.IOException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
-import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.context.ContextConfiguration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.study.eda.shared.support.jackson.config.EncryptionConfiguration;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @JsonTest
 @ContextConfiguration(classes = EncryptionConfiguration.class)
 public class EncryptionTest {
 
 	@Autowired
-	private JacksonTester<FooDTO> jacksonTester;
+	private ObjectMapper objectMapper;
 
 	@Test
 	void encryption() throws IOException {
 		// given
 		final var text = "test";
-		final var value = new FooDTO(text);
-		final var encrypted = EncryptionHelper.INSTANCE.encodeBase64(text.getBytes());
-		final var encryptedString = new String(encrypted);
+		final var value = FooDTO.from(text);
+		final var expected = """
+			{"text":"dGVzdA=="}
+			""";
 
 		// when
-		final var json = jacksonTester.write(value);
+		final var actual = objectMapper.writeValueAsString(value);
 
 		// then
-		assertThat(json).extractingJsonPathStringValue("$.text").isEqualTo(encryptedString);
+		assertThat(actual).isEqualToIgnoringWhitespace(expected);
 	}
 
-	record FooDTO(@Encryption String text) {
+	@Test
+	void decryption() throws IOException {
+		// given
+		final var json = """
+			{"text":"dGVzdA=="}
+			""";
+		final var expected = "test";
+
+		// when
+		final var dto = objectMapper.readValue(json, FooDTO.class);
+
+		// then
+		assertThat(dto.getText()).isEqualTo(expected);
+	}
+
+	@Getter
+	@Setter
+	static class FooDTO {
+		@Encryption
+		private String text;
+
+		public static FooDTO from(String text) {
+			final var dto = new FooDTO();
+			dto.setText(text);
+			return dto;
+		}
 	}
 }
